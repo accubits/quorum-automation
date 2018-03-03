b8f9764530be675e9bef7b153adc9b77a1e14caa
goutham krishna <krish4gouthu@gmail.com>
2018-03-03 12:40:08 +0530

    test

--- quorum.sh
+++ quorum.sh
@@ -8,10 +8,14 @@ cd $1
 #getting the nessecary files from git repo
 apt-get install git
 apt-get update
-git  clone https://github.com/Szkered/quorum-raft-cluster.git
+git  clone https://gitlab.com/goutham_krishna/quorum.git
 #change directory isn not allowed 
 change quorum-raft-cluster
 #script for getting quorum
 ./get-quorum.sh
 #script for setting up raft
 ./raft-setup.sh
+#script for initialising the quorum
+./raft-init.sh 
+./constellation-start.sh
+./raft-start.sh
