d9e6f9b9090709f883ba5dbc50a89275c5f4cf99
goutham krishna <krish4gouthu@gmail.com>
2018-03-03 22:29:59 +0530

    changes

--- quorum.sh
+++ quorum.sh
@@ -21,4 +21,5 @@ change quorum
 ./raft-start.sh
 #read -p $'\e[1;31mPlease enter this node\' IP Address: \e[0m' ip
 #read -p $'\e[1;31mPlease enter this node\' enode hash without domain: \e[0m' enode
+change quorum
 echo "raft.addPeer('enode://8dc1210d3d9cdb606b067a162db45e535f481663fda16bef717bad778b3631d5b017fb554a879d9fc8d006713195c0a7cd280a5ee2a72ada671a01d6207ba1cb@54.169.44.78:21000?discport=0&raftport=23000')"| geth attach quorum/qdata/geth.ipc 
