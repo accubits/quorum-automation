b3c011cef2f721fc53a3c349dee8709d4a96e640
root <root@ip-172-31-23-10.ap-southeast-1.compute.internal>
2018-03-04 05:42:52 +0000

    added waittime for script run

--- quorum.sh
+++ quorum.sh
@@ -10,6 +10,7 @@ read -p $'\e[1;31mPlease enter this node\' IP Address: \e[0m' ip
 read -p $'\e[1;31mPlease enter this node\' enode hash without domain: \e[0m' enode
 }
 #getting the nessecary files from git repo
+#readdata
 apt-get install git
 apt-get update
 git  clone https://gitlab.com/goutham_krishna/quorum.git
@@ -20,9 +21,10 @@ change quorum
 #script for setting up raft
 ./raft-setup.sh
 #script for initialising the quorum
-./raft-init.sh 
+./raft-init.sh
+sleep 5
 ./constellation-start.sh
+sleep 5
 ./raft-start.sh
-
-readdata
+sleep 5
 echo "raft.addPeer('enode://8dc1210d3d9cdb606b067a162db45e535f481663fda16bef717bad778b3631d5b017fb554a879d9fc8d006713195c0a7cd280a5ee2a72ada671a01d6207ba1cb@54.169.44.78:21000?discport=0&raftport=23000')"| geth attach /home/ubuntu/script-quorum/quorum/qdata/geth.ipc 
