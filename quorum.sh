6be4f15fc365017854f47cc7343808bc974a3671
root <root@ip-172-31-23-10.ap-southeast-1.compute.internal>
2018-03-04 13:16:47 +0000

    added condition check for master and child node

--- quorum.sh
+++ quorum.sh
@@ -11,6 +11,12 @@ read -p $'\e[1;31mPlease enter this node\' enode hash without domain: \e[0m' eno
 }
 #getting the nessecary files from git repo
 #readdata
+if [ $# -ne 1 ]
+then :
+echo "Please input a valid argument -m (Master Node) or -c (Child Node)"
+
+elif [ $1 = '-c' ] || [ $1 = '-m' ]
+then :
 apt-get install git
 apt-get update
 apt-get install jq
@@ -28,6 +34,14 @@ sleep 5
 sleep 5
 ./raft-start.sh
 sleep 5
+if [ $1 = '-c' ]
+
+then :
 jq -c '.[]' $PWD/../input_nodes.json | while read i; do
-	echo "raft.addPeer("$i")"| geth attach $PWD/qdata/geth.ipc 
+	echo "raft.addPeer("$i")"| geth attach $PWD/qdata/geth.ipc
 done
+fi
+
+else :
+echo "Please input a valid argument -m (Master Node) or -c (Child Node)"
+fi
