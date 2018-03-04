1f4438d04d95dfb65eec8251a6445f5895a7a8f2
goutham krishna <krish4gouthu@gmail.com>
2018-03-04 06:51:22 +0530

    adding inputs

--- quorum.sh
+++ quorum.sh
@@ -5,6 +5,10 @@ function change(){
 echo "traversing into the folder"
 cd $1
 }
+function readdata(){
+read -p $'\e[1;31mPlease enter this node\' IP Address: \e[0m' ip
+read -p $'\e[1;31mPlease enter this node\' enode hash without domain: \e[0m' enode
+}
 #getting the nessecary files from git repo
 apt-get install git
 apt-get update
@@ -19,7 +23,6 @@ change quorum
 ./raft-init.sh 
 ./constellation-start.sh
 ./raft-start.sh
-#read -p $'\e[1;31mPlease enter this node\' IP Address: \e[0m' ip
-#read -p $'\e[1;31mPlease enter this node\' enode hash without domain: \e[0m' enode
-change quorum
-echo "raft.addPeer('enode://8dc1210d3d9cdb606b067a162db45e535f481663fda16bef717bad778b3631d5b017fb554a879d9fc8d006713195c0a7cd280a5ee2a72ada671a01d6207ba1cb@54.169.44.78:21000?discport=0&raftport=23000')"| geth attach quorum/qdata/geth.ipc 
+
+readdata
+echo "raft.addPeer('enode://8dc1210d3d9cdb606b067a162db45e535f481663fda16bef717bad778b3631d5b017fb554a879d9fc8d006713195c0a7cd280a5ee2a72ada671a01d6207ba1cb@54.169.44.78:21000?discport=0&raftport=23000')"| geth attach /home/ubuntu/script-quorum/quorum/qdata/geth.ipc 
