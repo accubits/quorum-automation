b0548a09f4d21c27468993df50611a35e40eb45c
goutham krishna <krish4gouthu@gmail.com>
2018-03-07 13:04:23 +0530

    new module

--- quorum.sh
+++ quorum.sh
@@ -6,50 +6,88 @@ echo "traversing into the folder"
 cd $1
 }
 function readdata(){
-read -p $'\e[1;31mPlease enter this node\' IP Address: \e[0m' ip
+read -p $'\e[1;31mSetup master node 2\' IP Address: \e[0m' ip
 read -p $'\e[1;31mPlease enter this node\' enode hash without domain: \e[0m' enode
 }
 #getting the nessecary files from git repo
 #readdata
 regex="\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
-if [ $# -ne 2 ]
+if [ $# -ne 3 ]
 then :
-echo "Please input a valid argument -m (Master Node) or -c (Child Node) and public ip address of mongo server"
-elif [ -z $(grep -oE "\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b" <<< $2 ) ]
-then :
-echo "Please input a valid public ip address of mongo server"
-elif [ $1 = '-c' ] || [ $1 = '-m' ]
-then :
-apt-get install git
-apt-get update
-apt-get install jq
-git  clone https://gitlab.com/goutham_krishna/quorum.git
-#change directory isn not allowed 
-change quorum
-#script for getting quorum
-./get-quorum.sh
-#script for setting up raft
-./raft-setup.sh
-#script for initialising the quorum
-./raft-init.sh
-sleep 5
-./constellation-start.sh
-sleep 5
-./raft-start.sh
-sleep 5
-if [ $1 = '-c' ]
-then :
-curl -X GET  http://$2:8020/api/v1/getBootNodes -o $PWD/../input_nodes.json
-jq -c '.[]' $PWD/../input_nodes.json | while read i; do
-	echo "raft.addPeer("$i")"| geth attach $PWD/qdata/geth.ipc
-done
-else :
-jq -c '.[]' $PWD/raft/static-nodes.json | while read i; do
-      	temp="${i%\"}"
-	temp="${temp#\"}"
-	curl -X POST http://$2:8020/api/v1/addNode -H 'content-type: application/x-www-form-urlencoded' --data-urlencode 'enode='$temp -d 'typeNode=1'
-done
-fi
-else :
-echo "Please input a valid argument -m (Master Node) or -c (Child Node)"
+	echo "Please input a valid argument -m (Master Node) masternode number (1 or 2) and public ip address of mongo server"
+ elif [ -z $(grep -oE "\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b" <<< $3 ) ]
+ then :
+	echo "Please input a valid public ip address of mongo server"
+ elif [ $1 = '-m' ] || [ $1 ='-c' ]
+ then :
+	apt-get install git
+   	apt-get update
+	apt-get install jq
+	git  clone https://gitlab.com/goutham_krishna/quorum.git 
+	change quorum
+    ./get-quorum.sh
+	./raft-setup.sh
+	#master node 1 
+	if [ $1='-m' ]  &&  [ $2 = '1' ]
+	then :
+		jq -c '.[]' $PWD/raft/static-nodes.json | while read i; do
+      	  	temp="${i%\"}"
+	      	temp="${temp#\"}"
+	      	curl -X POST http://$3:8020/api/v1/addNode -H 'content-type: application/x-www-form-urlencoded' --data-urlencode 'enode='$temp -d 'typeNode=1'
+        done
+    	read -p $'\e[1;31mSetup master node 2 2\' Press y to continue: \e[0m' choice
+		echo $choice
+        if [ choice ='y' ] || [ choice='Y' ]
+        then :
+		    sed -i d raft/static-nodes.json
+			sleep 1
+			chmod 777 raft/static-nodes.json
+			sleep 2
+			curl http://$3:8020/api/v1/getBootNodes >>$PWD/raft/static-nodes.json
+			sleep 5
+        	./raft-init.sh
+        	sleep 5
+        	./constellation-start.sh
+            sleep 5
+            ./raft-start.sh
+            sleep 5
+	   fi
+	 elif [ $1='-m' ] && [ $2='2' ]
+	 then :
+	    echo "entered the if condition"
+	 	jq -c '.[]' $PWD/raft/static-nodes.json | while read i; do
+      	  	temp="${i%\"}"
+	      	temp="${temp#\"}"
+	      	curl -X POST http://$3:8020/api/v1/addNode -H 'content-type: application/x-www-form-urlencoded' --data-urlencode 'enode='$temp -d 'typeNode=1'
+        done
+		sleep 2
+		sed -i d raft/static-nodes.json
+		sleep 1
+		chmod 777 raft/static-nodes.json
+		sleep 3
+		curl http://$3:8020/api/v1/getBootNodes >>$PWD/raft/static-nodes.json
+		sleep 4
+        ./raft-init.sh
+        sleep 5
+        ./constellation-start.sh
+        sleep 5
+        ./raft-start.sh
+        sleep 5
+          
+    
+ else :
+    echo "your enode value is"
+    cat $PWD/raft/static-nodes.json
+	read -p $'\e[1;31mEnter your raftid\' IP Address: \e[0m' raftid
+	./raft-init.sh
+    sleep 5
+    ./constellation-start.sh
+    sleep 5
+    ./raft-start.sh $raftid
+    sleep 5
+
+
+  fi
+ #else :
+ #echo "Please input a valid argument -m (Master Node) or -c (Child Node)"
 fi
