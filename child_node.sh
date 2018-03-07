8e8addf8de567cbd9ede952a8a56793ac85226d6
root <root@ip-172-31-3-219.ap-southeast-1.compute.internal>
2018-03-07 08:12:20 +0000

    executable

--- child_node.sh
+++ child_node.sh
@@ -12,13 +12,8 @@ read -p $'\e[1;31mPlease enter this node\' enode hash without domain: \e[0m' eno
 #getting the nessecary files from git repo
 #readdata
 regex="\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
-if [ $# -ne 3 ]
-then :
-	echo "Please input a valid argument -m (Master Node) masternode number (1 or 2) and public ip address of mongo server"
- elif [ -z $(grep -oE "\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b" <<< $3 ) ]
- then :
-	echo "Please input a valid public ip address of mongo server"
- elif [ $1 = '-m' ] || [ $1 ='-c' ]
+
+ if [ $1='-c' ]
  then :
 	apt-get install git
    	apt-get update
@@ -38,5 +33,5 @@ then :
     sleep 5
 
  else :
- echo "Please input a valid argument -m (Master Node) or -c (Child Node)"
+ echo "Please input a valid argument  -c (Child Node)"
 fi
